fd079709070efcb67fe3bef1a093226a8cd59958
Greg Testo <gregt@pursuit.org>
2022-03-28 10:50:57 -0400

    added ship validator

--- index.js
+++ index.js
@@ -16,48 +16,107 @@
 // find hortizontal tiles,
 
 // sub-problems
-// validate correct shape of ships
-// validate hortizontal, vertical and diagnal not touching rule
-// validate correct size of ships
+// validate correct shape of ships X
+// validate hortizontal, vertical and diagnal not touching rule X
+// validate correct size of ships X
 // validate correct total number of ships
 // validate correct kind of ships
 
+// let tilesVisited = ["0,1"];
+
+// let connectedTiles = {
+// 	"0,1": ["0,2"],
+// };
+
 function validateBattlefield(board) {
-	// let row = 5;
-	// let column = 5;
-	// let current = board[row][column];
-	// let left = board[row][column-1];
-	// let right = board[row][column+1];
-	// let bottom = board[row+1][column];
-	// let top = board[row-1][column];
-	// let values = [current, left, top, right, bottom];
+	let connectedTiles = {};
 
 	for (let i = 0; i < board.length; i++) {
+		let tilesTakenUp = 0;
+		let coordObj = {};
 		for (let j = 0; j < board[i].length; j++) {
 			let current = board[i][j];
-			let left = board[i][j - 1] === undefined ? null : board[i][j - 1];
-			let right = board[i][j + 1] === undefined ? null : board[i][j + 1];
-			let bottom = board[i + 1] ? board[i + 1][j] : null;
-			let top = board[i - 1] ? board[i - 1][j] : null;
 
-			// let topLeft = board[i - 1][j - 1];
-			// let topRight = board[i - 1][j + 1];
-			// let bottomLeft = board[i + 1][j - 1];
-			// let bottomRight = board[i + 1][j - 1];
+			let left = board[i][j - 1];
+			let top = board[i - 1]?.[j];
+			let right = board[i][j + 1];
+			let bottom = board[i + 1]?.[j];
 
-			// let values = [current, left, top, right, bottom];
+			let topLeft = board[i - 1]?.[j - 1];
+			let topRight = board[i - 1]?.[j + 1];
+			let bottomLeft = board[i + 1]?.[j - 1];
+			let bottomRight = board[i + 1]?.[j - 1];
 
-			// console.log(current, top, right)
-			if (current && (top || bottom) && (left || right)) {
-				return false;
+			if (current === 1) {
+				let coord = i + "," + j;
+				let adjTiles = [];
+
+				left && adjTiles.push(i + "," + (j - 1));
+				top && adjTiles.push(i - 1 + "," + j);
+				right && adjTiles.push(i + "," + (j + 1));
+				bottom && adjTiles.push(i + 1 + "," + j);
+				connectedTiles[coord] = adjTiles;
+
+				coordObj[i] = coordObj[i] ? [...coordObj[i], j] : [j];
+				tilesTakenUp++;
+				if (tilesTakenUp > 4) {
+					return false;
+				}
+			} else {
+				tilesTakenUp = 0;
 			}
-			// current && right && bottom
-			// current && top && left
-			// console.log(values);
+
+			if (current) {
+				if ((top || bottom) && (left || right)) {
+					// No vertical/hortizontal combination
+					return false;
+				}
+				if (topLeft || topRight || bottomLeft || bottomRight) {
+					// No Diagnal touching
+					return false;
+				}
+			}
 		}
 	}
+
+	let tilesVisited = [];
+
+	let result = Object.keys(connectedTiles).reduce((accum, curr, index) => {
+		let stack = [curr];
+		while (stack.length) {
+			let temp = stack.pop();
+			if (!connectedTiles[temp].length) accum[index] = [temp];
+			for (let adj of connectedTiles[temp]) {
+				if (!tilesVisited.includes(adj)) {
+					tilesVisited.push(adj);
+					stack.push(adj);
+					accum[index] = accum[index] ? [...accum[index], temp] : [temp];
+				}
+			}
+		}
+		return accum;
+	}, {});
+
+	// for (let tile in connectedTiles) {
+	// 	if (!tilesVisited.includes(tile)) {
+	// 		console.log(connectedTiles[tile]);
+	// 	}
+	// }
+	// console.log(connectedTiles);
 	return true;
 }
+
+// { 0: [ '0,0', '1,0', '1,0', '2,0' ],
+//   1: [ '0,5', '0,6' ],
+//   4: [ '1,2', '2,2' ],
+//   5: [ '1,8', '2,8' ],
+//   8: [ '2,4', '2,5', '2,5' ],
+//   13: [ '4,8' ],
+//   14: [ '5,4', '5,5', '5,5' ],
+//   17: [ '6,8' ],
+//   18: [ '7,3' ],
+//   19: [ '8,7' ] }
+
 let result = validateBattlefield([
 	[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
 	[1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
@@ -70,4 +129,5 @@ let result = validateBattlefield([
 	[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]);
+
 console.log(result);
